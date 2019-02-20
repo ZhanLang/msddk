7c34223a4d9c795602865d088cfcfd51506795e6
maguojun <maguojun123@126.com>
2019-02-20 14:32:38 +0800

    修改workitem使其更加优雅

--- kernel/include/kthread/workitem.h
+++ kernel/include/kthread/workitem.h
@@ -8,41 +8,37 @@ class CKeWorkItem
 {
 	typedef NTSTATUS (T::*P_INVOKER)( LPVOID lpParam);
 public:
-	CKeWorkItem(PDEVICE_OBJECT OB, T* pOwer, P_INVOKER Invoker)
+	CKeWorkItem(PDEVICE_OBJECT pDeviceObject, T* pOwer, P_INVOKER Invoker)
 	{
 		m_pOwer = pOwer;
 		m_pInvoker = Invoker;
 		m_lpParam = NULL;
-		m_pIoWorkItem = IoAllocateWorkItem(OB);
-
+		m_pDeviceObject = pDeviceObject;
 	}
 	~CKeWorkItem()
 	{
-		IoFreeWorkItem(m_pIoWorkItem);
 	}
 public:
 	NTSTATUS Exec(LPVOID lpParam)
 	{
 		m_lpParam = lpParam;
-		IoQueueWorkItem(m_pIoWorkItem, 
-			(PIO_WORKITEM_ROUTINE)WorkItemFunc,
-			DelayedWorkQueue,
-			this);
+		PIO_WORKITEM pIoWorkItem = IoAllocateWorkItem(m_pDeviceObject);
+		IoQueueWorkItem(pIoWorkItem,WorkItemFunc,DelayedWorkQueue,this);
 		m_event.Wait();
-		return m_Ret;
+		IoFreeWorkItem(pIoWorkItem);
+		return m_st;
 	}
 
 private:
 	static VOID WorkItemFunc(PDEVICE_OBJECT  DeviceObject,PVOID  Context)
 	{
 		UNREFERENCED_PARAMETER(DeviceObject);
-		CKeWorkItem* param = (CKeWorkItem*)Context;
-		param->OnCallBack();
+		((CKeWorkItem*)Context)->OnCallBack();
 	}
 
 	VOID OnCallBack()
 	{
-		m_Ret = (static_cast<T *>(m_pOwer)->*m_pInvoker)(m_lpParam);
+		m_st = (static_cast<T *>(m_pOwer)->*m_pInvoker)(m_lpParam);
 		m_event.Set();
 	}
 
@@ -51,8 +47,8 @@ private:
 	void* m_pOwer;
 	LPVOID m_lpParam;
 	P_INVOKER m_pInvoker;
-	PIO_WORKITEM m_pIoWorkItem;
-	NTSTATUS m_Ret;
+	NTSTATUS m_st;
+	PDEVICE_OBJECT m_pDeviceObject;
 };
 
 
